1818bf5e552ce6cef1d341c416d2c14651cdf36c
AbstractEyes <abstractpowered@outlook.com>
2022-07-26 12:19:30 -0700

    Readme fix and vs files.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Abscript.Parser;
+
+Console.WriteLine("Hello, World!");
+
+string Location = @"C:\Users\PHIL\source\repos\Abscript\Test\Abscript.abp";
+
+Tokenizer tokenize = new Tokenizer(File.ReadAllText(Location));
